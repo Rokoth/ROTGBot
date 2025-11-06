e2c4334ab22680a1281852a71da98fd504bfb529
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2025-11-06 23:30:11 +0500

    ref

--- App/ROTGBot/Contract/Interfaces/IEntity.cs
+++ App/ROTGBot/Contract/Interfaces/IEntity.cs
@@ -1,9 +1,13 @@
-﻿using System;
-
-namespace ROTGBot.Contract.Interfaces
+﻿namespace ROTGBot.Contract.Interfaces
 {
+    /// <summary>
+    /// Интерфейс базовой модели
+    /// </summary>
     public interface IEntity
     {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
         Guid Id { get; set; }
     }
 }
