5b7a1a30ded957e5ae9cac4f4854b0f2ae9d29a2
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2025-11-07 18:15:29 +0500

    [Feature-0.0.13] Веб-интерфейс. Отчёты

---
+++ App/ROTGBot/ROTGBot/Pages/Reports/ModeratorReport.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ROTGBot.Pages.Reports
+{
+    public class ModeratorReportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
