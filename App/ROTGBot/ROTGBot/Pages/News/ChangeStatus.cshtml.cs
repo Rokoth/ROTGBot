9cd9bb48e9f7b37af1ee8f08f17332b24dfd83be
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2025-11-10 18:39:38 +0500

    Старницы списка и смены статуса обращений

---
+++ App/ROTGBot/ROTGBot/Pages/News/ChangeStatus.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ROTGBot.Pages.News
+{
+    public class ChangeStatusModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- App/ROTGBot/ROTGBot/Pages/News/Index.cshtml.cs
+++ App/ROTGBot/ROTGBot/Pages/News/Index.cshtml.cs
@@ -7,16 +7,26 @@ using ROTGBot.Service;
 
 namespace ROTGBot.Pages.News
 {
-    public class IndexModel(ILogger<IndexModel> logger, INewsDataService newsDataService) : PageModel
+    public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger = logger;
-        private readonly INewsDataService _newsDataService = newsDataService;
+        private readonly ILogger<IndexModel> _logger;
+        private readonly INewsDataService _newsDataService;
+
+        public IndexModel(ILogger<IndexModel> logger, INewsDataService newsDataService)
+        {
+            _logger = logger;
+            _newsDataService = newsDataService;
+            Filter = new Contract.Filters.NewsFilter()
+            {
+
+            };
+        }
 
         [BindProperty]
         public List<Contract.Model.News> News { get; set; } = default!;
 
         [BindProperty]
-        public List<Contract.Filters.NewsFilter> News { get; set; } = default!;
+        public Contract.Filters.NewsFilter Filter { get; set; } = default!;
 
         public async Task<IActionResult> OnGet()
         {
