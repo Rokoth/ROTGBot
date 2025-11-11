9b5b495e31472ae4b2a4dd280540e72130e15d80
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2025-11-11 15:05:59 +0500

    Отчёт по модератрам

---
+++ App/ROTGBot/ROTGBot/Pages/Reports/AdminModeratorReport.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ROTGBot.Pages.Reports
+{
+    public class AdminModeratorReportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
