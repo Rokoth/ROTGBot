3b9262825b06a0f172f7bb36d1d2c3260b5c19c4
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2025-11-15 18:07:30 +0500

    Фильтр обращений

---
+++ App/ROTGBot/Contract/Filters/NewsFilter.cs
@@ -0,0 +1,11 @@
+﻿using ROTGBot.Contract.Model;
+
+namespace ROTGBot.Contract.Filters
+{
+    public class NewsFilter : Filter<News>
+    {
+        public NewsFilter(string name, int userNumber,  int? size, int? page, string sort) : base(size, page, sort)
+        {
+        }
+    }
+}
