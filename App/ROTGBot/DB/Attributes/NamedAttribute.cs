00a2e5d48c46d8cff4109d8b3e83f1afdac6df1f
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2025-11-17 15:57:07 +0500

    ref

---
+++ App/ROTGBot/DB/Attributes/NamedAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace ROTGBot.Db.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class NamedAttribute(string name) : Attribute
+    {
+        /// <summary>
+        /// Имя колоник БД
+        /// </summary>
+        public string Name { get; } = name;
+    }
+}
