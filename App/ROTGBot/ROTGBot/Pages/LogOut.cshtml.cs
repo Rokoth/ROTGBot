50aa666d89957433160e195effc5664388128f56
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2025-11-30 22:39:49 +0500

    Форма выхода

---
+++ App/ROTGBot/ROTGBot/Pages/LogOut.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ROTGBot.Pages
+{
+    public class LogOutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
