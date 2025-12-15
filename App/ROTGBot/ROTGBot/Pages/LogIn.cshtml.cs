e816e90da280febdb399aa7ab8acaf0f21f9b871
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2025-12-15 19:20:41 +0500

    Окно логина

--- App/ROTGBot/ROTGBot/Pages/LogIn.cshtml.cs
+++ App/ROTGBot/ROTGBot/Pages/LogIn.cshtml.cs
@@ -5,6 +5,11 @@ namespace ROTGBot.Pages
 {
     public class LogInModel : PageModel
     {
+        public string Error { get; set; } = default!;
+        public string Login { get; set; } = default!;
+        public string Password { get; set; } = default!;
+        public bool LoginSended { get; set; } = default!;
+
         public void OnGet()
         {
         }
