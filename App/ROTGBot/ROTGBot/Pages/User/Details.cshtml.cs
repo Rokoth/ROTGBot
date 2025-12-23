8668372b0a37fccb52ad92aa06f0f94ca66378c5
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2025-12-23 16:15:21 +0500

    Добавление роли, детализация пользователя. Тесты

--- App/ROTGBot/ROTGBot/Pages/User/Details.cshtml.cs
+++ App/ROTGBot/ROTGBot/Pages/User/Details.cshtml.cs
@@ -1,12 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ROTGBot.Service;
 
 namespace ROTGBot.Pages.User
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly IUserDataService _userDataService;
+        private readonly ILogger<UnblockUserModel> _logger;
+
+        public DetailsModel(IUserDataService userDataService, ILogger<UnblockUserModel> logger)
         {
+            _userDataService = userDataService;
+            _logger = logger;
+        }
+
+        public Contract.Model.User UserModel { get; set; } = default!;
+        public string Error { get; set; } = default!;
+        public bool IsError { get; set; } = false;
+
+        public async Task<IActionResult> OnGetAsync(Guid id)
+        {
+            var user = await _userDataService.GetUser(id, new CancellationToken());
+            if (user == null)
+                return NotFound();
+
+
+            return Page();
         }
     }
 }
--- App/ROTGBot/XUnitTests/TelegramMessageHandlerTests.cs
+++ App/ROTGBot/XUnitTests/TelegramMessageHandlerTests.cs
@@ -47,5 +47,35 @@ namespace XUnitTests
 
             Assert.Equal(5, result);
         }
+
+        /// <summary>
+        /// 0.0.22.2.4
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task HandleUpdates_Success_2_Async()
+        {
+            var wrapperService = new Mock<ITelegramBotWrapper>();
+            var groupsDataService = new Mock<IGroupsDataService>();
+            var userDataService = new Mock<IUserDataService>();
+            var newsDataService = new Mock<INewsDataService>();
+            var buttonDataService = new Mock<IButtonsDataService>();
+            var configuration = new Mock<IConfiguration>();
+            var logger = new Mock<ILogger<TelegramMessageHandler>>();
+
+            wrapperService.Setup(s => s.GetUpdatesAsync(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => [
+                    new Update()
+                    {
+                        UpdateId = 4
+                    }
+                ]);
+
+            var tgMainService = new TelegramMessageHandler(logger.Object, groupsDataService.Object, userDataService.Object, newsDataService.Object, buttonDataService.Object, configuration.Object, wrapperService.Object);
+
+            var result = await tgMainService.HandleUpdates(new List<Update>(), CancellationToken.None);
+
+            Assert.Equal(5, result);
+        }
     }
 }
