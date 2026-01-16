adb768041d7017f49754dabeef279bae2a153acf
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2026-01-16 17:26:35 +0500

    Разблокировка пользователя

--- App/ROTGBot/ROTGBot/Pages/User/UnblockUser.cshtml.cs
+++ App/ROTGBot/ROTGBot/Pages/User/UnblockUser.cshtml.cs
@@ -4,16 +4,10 @@ using ROTGBot.Service;
 
 namespace ROTGBot.Pages.User
 {
-    public class UnblockUserModel : PageModel
+    public class UnblockUserModel(IUserDataService userDataService, ILogger<UnblockUserModel> logger) : PageModel
     {
-        private readonly IUserDataService _userDataService;
-        private readonly ILogger<UnblockUserModel> _logger;
-
-        public UnblockUserModel(IUserDataService userDataService, ILogger<UnblockUserModel> logger)
-        {
-            _userDataService = userDataService;
-            _logger = logger;
-        }
+        private readonly IUserDataService _userDataService = userDataService;
+        private readonly ILogger<UnblockUserModel> _logger = logger;
 
         public Contract.Model.User UserModel { get; set; } = default!;
         public string Error { get; set; } = default!;
@@ -22,6 +16,17 @@ namespace ROTGBot.Pages.User
         public async Task<IActionResult> OnGetAsync(Guid id)
         {
             var user = await _userDataService.GetUser(id, new CancellationToken());
+            if(user == null)
+            {
+                IsError = true;
+                return Page();
+            }
+            UserModel = new Contract.Model.User()
+            {
+                Name = user.Name,
+                TGLogin = user.TGLogin
+            };
+            return Page();
         }
     }
 }
