3e9c793f028440819862fed889fb0ab0a9e44f47
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2026-02-01 21:23:07 +0500

    Повтороная отправка обращения

--- App/ROTGBot/ROTGBot/Pages/News/ResendNews.cshtml.cs
+++ App/ROTGBot/ROTGBot/Pages/News/ResendNews.cshtml.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ROTGBot.Service;
@@ -11,18 +13,47 @@ namespace ROTGBot.Pages.News
 
         public ResendNewsModel(ILogger<ResendNewsModel> logger, INewsDataService newsDataService)
         {
-
+            _newsDataService = newsDataService;
+            _logger = logger;
         }
 
         public ROTGBot.Contract.Model.News News { get; set; } = default!;
 
 
-        public async Task<IActionResult> OnGetAsync()
+        public async Task<IActionResult> OnGetAsync(Guid id)
         {
+            var auth = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            if (!auth.Succeeded || string.IsNullOrEmpty(auth.Principal.Identity.Name))
+                return RedirectToPage("/Auth");
+            var userId = Guid.Parse(auth.Principal.Identity.Name);
+
+            //todo: проверить права на просмотр и отправку
+
+            var result = await _newsDataService.GetNewsById(id, new CancellationToken());
+
+            if(result == null)
+            {
+                return NotFound();
+            }
+
+            News = result;
+
+            return Page();
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
+            var auth = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            if (!auth.Succeeded || string.IsNullOrEmpty(auth.Principal.Identity.Name))
+                return RedirectToPage("/Auth");
+            var userId = Guid.Parse(auth.Principal.Identity.Name);
+
+
+            //todo: отправка
+
+
         }
     }
 }
