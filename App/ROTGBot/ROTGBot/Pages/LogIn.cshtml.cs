a235a04a715b80a37bef3ed576820b9d5cc4270d
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2026-03-18 15:34:19 +0500

    Страница входа

--- App/ROTGBot/ROTGBot/Pages/LogIn.cshtml.cs
+++ App/ROTGBot/ROTGBot/Pages/LogIn.cshtml.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ROTGBot.Service;
 
 namespace ROTGBot.Pages
 {
-    public class LogInModel : PageModel
+    public class LogInModel(IUserDataService userDataService) : PageModel
     {
+        private readonly IUserDataService _userDataService = userDataService;
+
         public string Error { get; set; } = default!;
         public string Login { get; set; } = default!;
         public string Password { get; set; } = default!;
@@ -24,7 +27,7 @@ namespace ROTGBot.Pages
             }
             else if(!PasswordSended)
             {
-
+                PasswordSended = true;
             }
             else
             {
