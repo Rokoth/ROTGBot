2351eed9bccfed234a56d01bd291bb10db91a92e
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2026-05-26 12:09:13 +0500

    Веб. Выход

--- App/ROTGBot/ROTGBot/Pages/LogOut.cshtml.cs
+++ App/ROTGBot/ROTGBot/Pages/LogOut.cshtml.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -18,7 +20,7 @@ namespace ROTGBot.Pages
 
         public async Task<IActionResult> OnPostAsync()
         {
-
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
 
             if (IsError)
             {
